cdc65a92fd1fdb2b56040c031c6f20a43c9519b0
Chris Morrow <morrowc@ops-netman.net>
2022-05-07 18:56:05 -0400

    Start adding functions to get the capture portion rolling.

--- client/client.go
+++ client/client.go
@@ -3,23 +3,58 @@
 package client
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/vladimirvivien/go4vl/v4l2"
 	"github.com/vladimirvivien/go4vl/v4l2/device"
+	"google.golang.org/grpc"
 )
 
+const (
+	maxMsgSize = 1024 * 1000 * 10 // 10mB
+	format     = v4l2.PixelFmtMJPEG
+)
+
+// Client holds all of the information about the running client.
 type Client struct {
-	dev *device.Device
-	id  string
+	dev      *device.Device
+	conn     *grpc.ClientConn
+	id       string
+	h        int
+	w        int
+	srvAddr  string
+	imgCount int64
 }
 
-func New(dev, id string) (Server, error) {
+func New(dev, srvAddr, id, string, h, w int) (Server, error) {
 	d, err := device.Open(dev)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open device: %v", err)
 	}
+
 	return &Server{
-		dev: d,
-		id:  id,
+		dev:      d,
+		conn:     nil,
+		id:       id,
+		h:        h,
+		w:        w,
+		srvAddr:  srvAddr,
+		imgCount: 0,
 	}
 }
+
+func (c *Client) newConn(ctx context.Context) error {
+	conn, err := grpc.Dial(
+		c.srvAddr,
+		grpc.WithInsecure(),
+		grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(maxMsgSize)),
+	)
+	if err != nil {
+		return fmt.Errorf("failed to make new connection: %v", err)
+	}
+	c.conn = conn
+	return nil
+}
+
+// func (c *Client) Setup
