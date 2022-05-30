e3902d9fb78b5291a46e717b0b5ee03d28a8eb79
Chris Morrow <morrowc@ops-netman.net>
2022-05-30 16:11:04 -0400

    Integrate a server side config to the server binary.

--- server/main.go
+++ server/main.go
@@ -6,11 +6,13 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	"github.com/golang/glog"
-	pgpb "github.com/morrowc/picam/proto/picam"
+	pb "github.com/morrowc/picam/proto/picam"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/prototext"
 )
 
 const (
@@ -18,33 +20,52 @@ const (
 )
 
 var (
-	port  = flag.Int("port", 9987, "Port upon which to listen.")
-	store = flag.String("store", "/tmp/camstore", "Storage location for image files.")
+	port   = flag.Int("port", 9987, "Port upon which to listen.")
+	store  = flag.String("store", "/tmp/camstore", "Storage location for image files.")
+	config = flag.String("config", "", "Configuration for the server.")
 )
 
 // server holds connection/client information necessary to operate a gRPC server.
 type server struct {
-	store string
-	port  int
-	pgpb.UnimplementedPiCamServer
+	store  string
+	port   int
+	config *pb.Config
+	pb.UnimplementedPiCamServer
 }
 
-func new(store string, port int) *server {
+func new(store string, port int, config *pb.Config) *server {
 	return &server{
-		store: store,
-		port:  port,
+		store:  store,
+		port:   port,
+		config: config,
 	}
 }
 
-func (s *server) SendImage(ctx context.Context, req *pgpb.Request) (*pgpb.Response, error) {
+func (s *server) SendImage(ctx context.Context, req *pb.Request) (*pb.Response, error) {
 	id := req.GetIdentifier()
 	img := req.GetImage()
 	glog.Infof("Got request from ID: %s img size: %d", id, len(img))
-	return &pgpb.Response{}, nil
+	return &pb.Response{}, nil
+}
+
+// readConfig reads the server configuration proto from disk.
+func readConfig(fn string) (*pb.Config, error) {
+	dat, err := os.ReadFile(fn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file(%s): %v", fn, err)
+	}
+	var cfg pb.Config
+	if err := prototext.Unmarshal(dat, &cfg); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config proto: %v", err)
+	}
+	return &cfg, nil
 }
 
 func main() {
 	flag.Parse()
+	if *config == "" {
+		log.Fatalf("Provide a config path.")
+	}
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		glog.Fatalf("failed to listen(): %v", err)
@@ -55,9 +76,14 @@ func main() {
 		grpc.MaxRecvMsgSize(maxMsgSize),
 		grpc.MaxSendMsgSize(maxMsgSize),
 	)
-	server := new(*store, *port)
+	config, err := readConfig(*config)
+	server := new(*store, *port, config)
+	fmt.Printf("Will listen on port: %d\n", server.config.GetPort())
+	for _, client := range server.config.GetClient() {
+		fmt.Printf("Id: %s store: %s\n", client.GetId(), client.GetStore())
+	}
 
-	pgpb.RegisterPiCamServer(s, server)
+	pb.RegisterPiCamServer(s, server)
 
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
